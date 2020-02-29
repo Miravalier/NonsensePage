ef30a6b0d4cf9f61d8f56e131f7896ca1f8ab57e
Benjamin Miramontes <benjamin@miramontes.dev>
2020-02-29 22:01:49 +0000

    Finished install.sh script

--- local/install.sh
+++ local/install.sh
@@ -4,10 +4,29 @@ if [[ $EUID != 0 ]]; then
     exit 1
 fi
 
-#add-apt-repository -y ppa:deadsnakes/ppa
-#apt-get update
+apt install -y gcc nginx make postgresql postgresql-server-dev-all
 
-apt install -y cpp
-apt install -y nginx
-apt install -y make
-apt install -y python3.7
+python3.7 --version
+if [[ $? != 0 ]]; then
+	add-apt-repository -y ppa:deadsnakes/ppa
+	apt-get update
+	apt install -y python3.7
+fi
+
+python3.7 -m pip --version
+if [[ $? != 0 ]]; then
+	curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+	python3.7 get-pip.py
+	rm get-pip.py
+fi
+
+python3.7 -m pip install websockets psycopg2-binary
+
+sudo -u postgres createuser root
+sudo -u postgres createdb -O root root
+sudo -u postgres createdb -O root dnd
+psql dnd -f sql/attr_schema.sql
+psql dnd -f sql/entity_schema.sql
+psql dnd -f sql/file_schema.sql
+psql dnd -f sql/message_schema.sql
+psql dnd -f sql/user_schema.sql
