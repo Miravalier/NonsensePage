0fa50562382f6e1644486a34ceeda0e3e36a870d
Benjamin Miramontes <benjamin@miramontes.dev>
2021-11-04 20:33:19 -0400

    Added icons, switched to scss, started theme

--- client/main.js
+++ client/main.js
@@ -11,6 +11,9 @@ if (process.platform === 'win32') {
 
 function createWindow() {
     const mainWindow = new BrowserWindow({
+        icon: path.join(__dirname, '../build/icon.png'),
+        width: 1200,
+        height: 800,
         webPreferences: {
             preload: path.join(__dirname, 'preload.js')
         }
--- client/preload.js
+++ client/preload.js
@@ -1,7 +1,8 @@
 const { contextBridge } = require("electron");
 const fs = require("fs");
+const path = require('path');
 
-fs.readFile('package.json', 'utf8', (err, data) => {
+fs.readFile(path.join(__dirname, '../package.json'), 'utf8', (err, data) => {
     if (err) {
         console.error(err);
         return;
