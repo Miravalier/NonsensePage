b920faeccde0fb5d071021ed0fdff8f763a6f0d8
Benjamin Miramontes <benjamin@miramontes.dev>
2024-03-31 22:09:27 -0500

    Removed toast dep, fixed fontawesome

--- frontend/src/notifications.ts
+++ frontend/src/notifications.ts
@@ -1,34 +1,13 @@
-import { Toastify } from "toastify";
-
-
 export function InfoToast(message: string) {
     console.info(message);
-    const toast = new Toastify({
-        text: message,
-        className: "info-toast",
-    });
-    toast.showToast();
-    return toast;
 }
 
 
 export function WarningToast(message: string) {
     console.warn(message);
-    const toast = new Toastify({
-        text: message,
-        className: "warning-toast",
-    });
-    toast.showToast();
-    return toast;
 }
 
 
 export function ErrorToast(message: string) {
     console.error(message);
-    const toast = new Toastify({
-        text: message,
-        className: "error-toast",
-    });
-    toast.showToast();
-    return toast;
 }
